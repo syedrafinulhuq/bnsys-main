5d8d29720708cd49f6469b135df71bbfcffce828
A. M. Rafinul Huq <85757029+syedrafinulhuq@users.noreply.github.com>
2025-11-29 22:52:29 +0600

    Trigger workflow after fixing path

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: 
-  [
+  base: './', // 👈 IMPORTANT for cPanel
+  plugins: [
     react(),
     tailwindcss()
   ],
+  build: {
+    outDir: 'dist', // default, but explicit
+  }
 })
